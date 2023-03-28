256499261cb66b15b20d77cfc7caddacbe4b19b1
lc0197 <lukas.christ@informatik.uni-augsburg.de>
2023-03-28 18:47:47 +0200

    to device

--- model.py
+++ model.py
@@ -105,9 +105,9 @@ def last_item_from_packed(packed, lengths):
     sum_batch_sizes = torch.cat((
         torch.zeros(2, dtype=torch.int64),
         torch.cumsum(packed.batch_sizes, 0)
-    ))
-    sorted_lengths = lengths[packed.sorted_indices]
-    last_seq_idxs = sum_batch_sizes[sorted_lengths] + torch.arange(lengths.size(0))
+    )).to(device)
+    sorted_lengths = lengths[packed.sorted_indices].to(device)
+    last_seq_idxs = sum_batch_sizes[sorted_lengths] + torch.arange(lengths.size(0)).to(device)
     last_seq_items = packed.data[last_seq_idxs]
     last_seq_items = last_seq_items[packed.unsorted_indices]
     return last_seq_items
